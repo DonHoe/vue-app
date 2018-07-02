41e22d6188b20b7945290fa3721eeb0f2ea1dccb
hd <hddg@gmail.com>
2018-07-02 17:23:46 +0800

    modify mock func

--- src/mock/index.js
+++ src/mock/index.js
@@ -2,16 +2,19 @@ import Mock from 'mockjs';
 
 export default {
   bootstrap() {
-    Mock.mock("/api/getData",{
+    Mock.mock("/api/getData", function (ops) {
+      console.log(ops);
+      return Mock.mock({
         'list|10': [{
-            'id|+1': 1,
-            'name':'@cname',
-            'region':'@region',
-            'province':'@province',
-            'city':'@city',
-            'cdate':'@datetime("yyyy-MM-dd HH:mm:ss")'
+          'id|+1': 1,
+          'name': '@cname',
+          'region': '@region',
+          'province': '@province',
+          'city': '@city',
+          'cdate': '@datetime("yyyy-MM-dd HH:mm:ss")'
         }],
-        'count|11-49':1
+        'count|11-49': 1
+      })
     });
   }
 };
