b2a4bb1e9ea6a0f62e46ad2c231c183ea81894cf
HoeDon <houdong0524@live.com>
2019-04-16 15:46:34 +0800

    change warn

--- src/page/zhihu.vue
+++ src/page/zhihu.vue
@@ -40,7 +40,7 @@
                     <el-table-column prop="cdate" label="日期"></el-table-column>
                     <el-table-column label="操作">
                         <template slot-scope="scope">
-                            <el-button size="mini">编辑</el-button>
+                            <el-button size="mini" @click="handleClick(scope.row)">编辑</el-button>
                             <el-button size="mini" type="danger">删除</el-button>
                         </template>
                     </el-table-column>
@@ -102,6 +102,9 @@ export default {
         var that = this;
         that.mCurrPage = val;
         that.pageChange();
+    },
+    handleClick:function(row){
+        window.console.log(row);
     }
   }
 };
