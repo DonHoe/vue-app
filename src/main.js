23715ed73b25d75c1410d4ac5e6bd63eb20a40d2
HoeDon <houdong0524@live.com>
2019-04-16 11:24:55 +0800

    add dev base url

--- src/main.js
+++ src/main.js
@@ -9,10 +9,11 @@ MockData.bootstrap();
 
 Vue.config.productionTip = false
 Vue.prototype.$ajax = axios
-Vue.prototype.$baseUrl = process.env.baseUrl;
+Vue.prototype.$baseUrl = process.env.NODE_ENV == 'production' ? "" : "http://localhost:8090";
 Vue.use(Vuex)
 Vue.use(ElementUI)
-
+window.console.log(process.env);
+window.console.log(Vue.prototype.$baseUrl);
 new Vue({
     render: h => h(App),
 }).$mount('#app')
