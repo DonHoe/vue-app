6b9d3b519966f94f3cf25817927047001089c618
HoeDon <houdong0524@live.com>
2019-07-31 15:32:33 +0800

    add qa content

--- src/page/zhihu.vue
+++ src/page/zhihu.vue
@@ -10,10 +10,10 @@
             <sc-search>
                 <el-form :inline="true" :model="searchModel" size="mini" label-width="100px">
                     <el-form-item label="标题">
-                        <el-input v-model="searchModel.title" class="input" ></el-input>
+                        <el-input v-model="searchModel.title" class="input"></el-input>
                     </el-form-item>
                     <el-form-item label="内容">
-                        <el-input v-model="searchModel.content" class="input" ></el-input>
+                        <el-input v-model="searchModel.content" class="input"></el-input>
                     </el-form-item>
                     <el-form-item label="开始时间">
                         <el-date-picker v-model="searchModel.startDate" type="datetime" value-format="yyyy-MM-dd HH:mm:ss" placeholder="选择日期时间">
@@ -33,11 +33,10 @@
             <div>
                 <el-table :data="list" border stripe size="small">
                     <el-table-column prop="id" label="Id"></el-table-column>
-                    <el-table-column prop="name" label="名字"></el-table-column>
-                    <el-table-column prop="region" label="区域"></el-table-column>
-                    <el-table-column prop="province" label="省份"></el-table-column>
-                    <el-table-column prop="city" label="城市"></el-table-column>
-                    <el-table-column prop="cdate" label="日期"></el-table-column>
+                    <el-table-column prop="author" label="作者"></el-table-column>
+                    <el-table-column prop="vote" label="赞同"></el-table-column>
+                    <el-table-column prop="comment" label="评论"></el-table-column>
+                    <el-table-column prop="thanks" label="感谢"></el-table-column>
                     <el-table-column label="操作">
                         <template slot-scope="scope">
                             <el-button size="mini" @click="handleClick(scope.row)">编辑</el-button>
@@ -47,65 +46,72 @@
                 </el-table>
             </div>
             <div style="margin-top: 10px;">
-                <el-pagination layout="total, sizes, prev, pager, next, jumper" 
-                :total="totalRecords" :current-page="mCurrPage" :page-size="mCurrPageSize"
-                @size-change="changeSize" @current-change="changeCurrent">
+                <el-pagination layout="total, sizes, prev, pager, next, jumper" :total="totalRecords" :current-page="mCurrPage" :page-size="mCurrPageSize" @size-change="changeSize" @current-change="changeCurrent">
                 </el-pagination>
             </div>
         </div>
+        <el-dialog title="内容" :visible.sync="contentDialogVisible" width="80%">
+            <div v-html="content"></div>
+        </el-dialog>
     </div>
 </template>
 <script>
 export default {
-  data() {
-    return {
-      searchModel: {
-        title: "",
-        content: "",
-        startDate: "",
-        endDate: ""
-      },
-      list: [],
-      totalRecords: 10,
-      mCurrPageSize:10,
-      mCurrPage:1
-    };
-  },
-  created:function(){
-      var that = this;
-      that.search();
-  },
-  methods: {
-    search: function() {
-      var that = this;
-      window.console.log(that.searchModel);
-      this.$ajax
-        .get("/api/getData")
-        .then(function(response) {
-          that.list = response.data.list;
-          that.totalRecords = response.data.count;
-        })
-        .catch(function(err) {
-          window.console.log(err);
-        });
+    data() {
+        return {
+            searchModel: {
+                title: "",
+                content: "",
+                startDate: "",
+                endDate: ""
+            },
+            list: [],
+            totalRecords: 10,
+            mCurrPageSize: 10,
+            mCurrPage: 1,
+            contentDialogVisible: false,
+            content: ""
+        };
     },
-    pageChange:function(){
+    created: function() {
         var that = this;
         that.search();
     },
-    changeSize:function(val){
-        var that = this;  
-        that.mCurrPageSize = val;
-        that.pageChange();
-    },
-    changeCurrent:function(val){
-        var that = this;
-        that.mCurrPage = val;
-        that.pageChange();
-    },
-    handleClick:function(row){
-        window.console.log(row);
+    methods: {
+        search: function() {
+            var that = this;
+            window.console.log(that.searchModel);
+            this.$ajax
+                .get(this.$baseUrl + "/qa/getList")
+                .then(function(response) {
+                    window.console.log(response);
+                    that.list = response.data.result;
+                    that.totalRecords = 100;
+                })
+                .catch(function(err) {
+                    window.console.log(err);
+                });
+        },
+        pageChange: function() {
+            var that = this;
+            that.search();
+        },
+        changeSize: function(val) {
+            var that = this;
+            that.mCurrPageSize = val;
+            that.pageChange();
+        },
+        changeCurrent: function(val) {
+            var that = this;
+            that.mCurrPage = val;
+            that.pageChange();
+        },
+        handleClick: function(row) {
+            window.console.log(row);
+            var that = this;
+            that.contentDialogVisible = true;
+            that.content = row.content;
+        }
     }
-  }
 };
 </script>
